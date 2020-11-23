ef750335a96b11e1418c5a03c4a9124b7c300fe1
Jacob Whisler <jwhisler521@gmail.com>
2020-11-22 20:45:07 -0500

    added piece of state to determine whether or not there is a song playing in order to update play/pause buttons

--- src/App.js
+++ src/App.js
@@ -11,11 +11,16 @@ function App() {
   // State
   const [songs, setsongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[4]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
