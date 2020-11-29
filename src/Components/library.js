59ed407cde164f22ee95ba35b5524900132d8aed
Jacob Whisler <jwhisler521@gmail.com>
2020-11-29 11:26:01 -0500

    added loop to map over all the songs to create a library song component for each song

--- src/Components/library.js
+++ src/Components/library.js
@@ -1,12 +1,14 @@
 import React from "react";
 import LibrarySong from "./librarySong";
 
-const library = () => {
+const library = ({ songs }) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
-        <LibrarySong />
+        {songs.map((song) => (
+          <LibrarySong song={song} />
+        ))}
       </div>
     </div>
   );
