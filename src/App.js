d45a9235548be0de617ae6295291e96812a5b446
Jacob Whisler <jwhisler521@gmail.com>
2020-11-29 19:39:26 -0500

    added functionality to make the songs in the song library clickable/song will change upon clicking new song in the song library

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
       />
-      <Library songs={songs} />
+      <Library setCurrentSong={setCurrentSong} songs={songs} />
     </div>
   );
 }
