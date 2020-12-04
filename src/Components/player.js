3aad0ebb3752014105921aadb11dc2e18a2b43ae
Jacob Whisler <jwhisler521@gmail.com>
2020-12-03 20:23:23 -0500

    attempted to customize input styles, need to come back later and work out bugs...original styling still in use currently

--- src/Components/player.js
+++ src/Components/player.js
@@ -40,11 +40,21 @@ const Player = ({
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration });
+    // calculate percentage
+    // const roundedCurrent = Math.round(current);
+    // const roundedDuration = Math.round(duration);
+    // const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      // animationPercentage: animation,
+    });
   };
 
   // State
@@ -96,10 +106,20 @@ const Player = ({
     setSongs(newSongs);
   }, [currentSong]);
 
+  // const trackAnim = {
+  //   transform: `translateX(${songInfo.animationPercentage}%)`,
+  // };
+
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
+        {/* <div
+        style={{
+          background: `linear-gradient(to right, ${currentSong.color[0]}, ${currentSong.color[1]})`,
+        }}
+        className="track"
+      > */}
         <input
           onChange={dragHandler}
           min={0}
@@ -107,6 +127,8 @@ const Player = ({
           value={songInfo.currentTime}
           type="range"
         />
+        {/* <div  className="animate-track" ></div> */}
+        {/* </div> */}
         <p>
           {songInfo.duration
             ? getTime(songInfo.duration - songInfo.currentTime)
